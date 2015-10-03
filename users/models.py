e48ebc660a8e9c81bda4b9814864f116e1461781
Alejandro Cuenca Estrada <alexcuenca4@gmail.com>
2015-10-03 17:46:54 -0500

    Create models for users and business

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseUser(models.Model):
+	username = models.CharField(max_length=140)
+	password = models.CharField(max_length=140)
+	email = models.CharField(max_length=140)
+
+	def __unicode__(self):
+		return self.username
+
+
+class BusinessUser(models.Model):
+	businessname = models.CharField(max_length=140)
+	type = models.CharField(max_length=140)
